45b55c1c3e81702cd00ee2ba53c8c41c2d0e0cd7
stkorney <nnet95435@gmail.com>
2026-03-08 12:29:11 +0300

    Delete src directory

--- src/logger/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.logger.cometml import CometMLWriter
-from src.logger.logger import setup_logging
-from src.logger.wandb import WandBWriter
--- src/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.model.baseline_model import BaselineModel
-from src.model.lcnn_model import LCNNModel
-
-__all__ = [
-    "BaselineModel",
-    "LCNNModel",
-]
